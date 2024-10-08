3ca38ff9705b1918d5a6496d8eae290267771454
v <lirelum@disroot.org>
2024-10-07 23:32:24 -0400

    Add steam

--- hosts/miku/configuration.nix
+++ hosts/miku/configuration.nix
@@ -80,6 +80,14 @@
     pulse.enable = true;
   };
 
+  # Gaming
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   # User config
   users.users.lirelum = {
     isNormalUser = true;
