980d2d434793ad7d5927f209848886840c171ba2
v <lirelum@disroot.org>
2024-10-15 19:48:11 -0400

    stuff

--- hosts/miku/configuration.nix
+++ hosts/miku/configuration.nix
@@ -102,20 +102,6 @@
   programs.nix-ld = {
     enable = true;
     libraries = with pkgs; [
-      zlib
-      zstd
-      stdenv.cc.cc
-      curl
-      openssl
-      attr
-      libssh
-      bzip2
-      libxml2
-      acl
-      libsodium
-      util-linux
-      xz
-      systemd
     ];
   };
 
