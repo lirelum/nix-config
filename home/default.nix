8599822245bfcb85c0a0545ad720c342423804d0
v <lirelum@disroot.org>
2024-10-25 19:59:08 -0400

    add retroarch

--- home/default.nix
+++ home/default.nix
@@ -1,53 +1,57 @@
-{ inputs, self, ... }:
 {
-  flake.homeModules.default =
-    { pkgs, ... }:
-    let
-      rstudio-custom = pkgs.unstable.rstudioWrapper.override {
-        packages = with pkgs.unstable.rPackages; [ ggplot2 ];
-      };
-    in
-    {
-      imports = [
-        ./shell
-        ./programs-helix
-        ./fonts
-        ./programs-gpg
-        ./programs-git
-        ./programs-firefox
-        ./programs-vscode
-      ];
-
-      home.packages =
-        with pkgs;
-        [
-          zip
-          unzip
-          zx
-          p7zip
-          file
-          which
-          btop
-          lsof
-          rstudio-custom
-        ]
-        ++ (
-          if pkgs.system == "x86_64-linux" then
-            [
-              (discord.override {
-                withVencord = true;
-                withOpenASAR = true;
-              })
-              libreoffice
-              calibre
-              thunderbird
-              nextcloud-client
-              keepassxc
-            ]
-          else
-            [ ]
-        );
-
-      home.stateVersion = "24.05";
+  inputs,
+  self,
+  ...
+}: {
+  flake.homeModules.default = {pkgs, ...}: let
+    rstudio-custom = pkgs.unstable.rstudioWrapper.override {
+      packages = with pkgs.unstable.rPackages; [ggplot2];
     };
+    retroarch' = pkgs.retroarch.override {
+      cores = with pkgs.libretro; [
+        snes9x
+      ];
+    };
+  in {
+    imports = [
+      ./shell
+      ./programs-helix
+      ./fonts
+      ./programs-gpg
+      ./programs-git
+      ./programs-firefox
+      ./programs-vscode
+    ];
+
+    home.packages = with pkgs;
+      [
+        zip
+        unzip
+        zx
+        p7zip
+        file
+        which
+        btop
+        lsof
+        rstudio-custom
+      ]
+      ++ (
+        if pkgs.system == "x86_64-linux"
+        then [
+          (discord.override {
+            withVencord = true;
+            withOpenASAR = true;
+          })
+          libreoffice
+          calibre
+          thunderbird
+          nextcloud-client
+          keepassxc
+          retroarch'
+        ]
+        else []
+      );
+
+    home.stateVersion = "24.05";
+  };
 }
