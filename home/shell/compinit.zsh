766856196e3985b5c762871606197af45f3813e0
v <lirelum@disroot.org>
2025-01-03 14:12:16 -0500

    Change completion init

--- home/shell/compinit.zsh
+++ home/shell/compinit.zsh
@@ -1,3 +1,4 @@
 zstyle ':completion:*' completer _complete _ignored _approximate
 zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}'
 zstyle :compinstall filename '/home/lirelum/.zshrc'
+autoload -U compinit && compinit
