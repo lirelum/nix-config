4c3344b73e1783a7c218a2fc69b67d9fa2cbd963
v <lirelum@disroot.org>
2025-01-04 05:13:22 -0500

    t Add nushell and carapace support

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -1,1 +1,10 @@
-{ imports = [ ./zsh.nix ./direnv.nix ./starship.nix ./aliases.nix ]; }
+{
+  imports = [
+    ./zsh.nix
+    ./direnv.nix
+    ./starship.nix
+    ./aliases.nix
+    ./nushell.nix
+    ./carapace.nix
+  ];
+}
