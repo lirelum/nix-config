3fe8268e2de0827ab799a2fde4403dc755d530ad
autumn <lirelum@disroot.org>
2025-01-06 05:29:59 -0500

    Enable avahi for mdns

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,11 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+  
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -71,7 +76,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
